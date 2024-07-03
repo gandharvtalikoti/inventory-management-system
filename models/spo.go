2dad9f18e3fa0ad1ea3a0e1d6e09edc66ccb943a
gandharv <gandharvwork@gmail.com>
2024-07-03 16:04:40 +0530

    refactor

--- models/spo.go
+++ models/spo.go
@@ -15,14 +15,6 @@ type SPO struct {
 }
 
 type SPOparams struct {
-	// MPOID           int       `json:"mpo_id"`
-	// InstanceID      string    `json:"instance_id"`
-	// WarehouseID     string    `json:"warehouse_id"`
-	// DOA             time.Time `json:"doa"`
-	// Status          string    `json:"status"`
-	// PDFFilename     string    `json:"pdf_filename"`
-	// InvoiceNumber   string    `json:"invoice_number"`
-	// Mpo_instance_id string    `json:"mpo_instance_id"`
 	Mpo models.MPOparams                      `json:"mpo"`
 	Spo models.SPOparams                      `json:"spo"`
 	Sku []models.PurchaseOrderInventoryParams `json:"sku"`
